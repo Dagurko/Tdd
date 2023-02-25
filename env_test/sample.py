3fe77787e9b727d22ac4f2323f481da4dc1449bd
Dagurko <dagurk@internet.is>
2023-02-25 16:42:44 +0000

    Delete test in sample

--- env_test/sample.py
+++ env_test/sample.py
@@ -26,9 +26,5 @@ def calculate_order(item_list, location):
 
 
 
-def test_single_item_no_taxes_or_shipping():
-    item_list = {'item_1': 1}
-    location = 'Iceland'
-    result = calculate_order(item_list, location)
-    assert result[1] == 10
+
 
