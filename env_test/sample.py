4848cf39507b3ba749047736fc8fb31153c976f4
Dagurko <dagurk@internet.is>
2023-02-25 16:42:02 +0000

    Fix sample and fith test work

--- env_test/sample.py
+++ env_test/sample.py
@@ -2,22 +2,24 @@ TAX_RATES = {'Iceland': 0.24,
              'France': 0.2, 
              'Denmark': 0.15 }
 
-SHIPPING_FEES = {'Iceland': {1: 14.75, 5: 24, 10: 39.25}, 
-                 'France': {1: 17.5 , 5: 28.7 , 10: 44.55}, 
-                 'Denmark': {1: 17.5 , 5: 28.7 , 10: 44.55}}
+SHIPPING_FEES = {'Iceland': {1: 14.75, 2: 16.35, 4: 21.35, 5: 24, 6: 27.05, 7: 30.10, 8: 33.15, 9: 36.2, 10: 39.25}, 
+                 'France': {1: 17.5, 2: 20.05, 3: 22.9, 4: 25.8, 5: 28.7, 6: 31.9, 7: 35.05, 8: 38.2, 9: 41.35, 10: 44.55}, 
+                 'Denmark': {1: 17.5, 2: 20.05, 3: 22.90, 4: 25.8, 5: 28.7, 6: 31.9, 7: 25.05, 8: 38.2, 9: 41.35, 10: 44.55}}
 
-ITEM = {'item_1': {'price': 10, 'weight': 1},
-        'item_2': {'price': 50, 'weight': 5}, 
-        'item_3': {'price': 35, 'weight': 1}, 
-        'item_4': {'price': 90, 'weight': 10}, 
-        'item_5': {'price':100, 'weight': 5}}
+ITEM = {'item_1': {'price': 10, 'weight': 2},
+        'item_2': {'price': 50, 'weight': 3}, 
+        'item_3': {'price': 35, 'weight': 5}, 
+        'item_4': {'price': 90, 'weight': 1}, 
+        'item_5': {'price':100, 'weight': 2}}
 
 def calculate_order(item_list, location):
     subtotal = 0
     shipping_fees = 0
+    weight = 0
     for item, quantity in item_list.items():
         subtotal += (ITEM[item]['price'] * quantity) 
-        shipping_fees += (SHIPPING_FEES[location][ITEM[item]['weight']]*quantity)
+        weight += ITEM[item]['weight'] * quantity
+    shipping_fees = SHIPPING_FEES[location][weight]
     tax = subtotal * TAX_RATES[location]
     total = subtotal + tax + shipping_fees
     return (total, subtotal, tax, shipping_fees)
--- env_test/test_sample.py
+++ env_test/test_sample.py
@@ -1,16 +1,12 @@
 import pytest
 import sample
-# ITEM = {'item_1': {'price': 10, 'weight': 1},
-#         'item_2': {'price': 50, 'weight': 5}, 
-#         'item_3': {'price': 35, 'weight': 1}, 
-#         'item_4': {'price': 90, 'weight': 10}, 
-#         'item_5': {'price':100, 'weight': 5}}
+
 
 def test_single_item_no_taxes_or_shipping():
-    item_list = {'item_1': 1}
+    item_list = {'item_3': 1}
     location = 'Iceland'
     result = sample.calculate_order(item_list, location)
-    assert result[1] == 10
+    assert result[1] == 35
 
 def test_multiple_items_no_taxes_or_shipping():
     item_list = {'item_1': 1, 'item_2': 1}
@@ -22,8 +18,20 @@ def test_calculate_taxes_by_location():
     item_list = {'item_1': 1}
     location = 'Iceland'
     result = sample.calculate_order(item_list, location)
-    assert result[2] == 10*0.24
+    assert result[2] == 2.4
+
+def test_calculate_shipping_by_location_and_weight():
+    item_list = {'item_1': 1, 'item_2': 1}
+    location = 'France'
+    result = sample.calculate_order(item_list, location)
+    assert result[3] == 28.7
 
 
+def test_total_cost_with_taxes_and_shipping():
+    item_list = {'item_1': 2, 'item_4': 3, 'item_5': 1}
+    location = 'France'
+    result = sample.calculate_order(item_list, location)
+    assert result[0] == 509.35
 
-    
+
+   
